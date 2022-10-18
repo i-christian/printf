eeeeb9cbcc1548022db8f81af7b4a02c6f474612
Christian <111076892+i-christian@users.noreply.github.com>
2022-10-18 20:42:14 +0200

    Update main.h

--- main.h
+++ main.h
@@ -23,9 +23,21 @@ int print_string(va_list);
 int print_percent(va_list);
 int print_integer(va_list list);
 int print_number(va_list);
-int print_binary(va_list);
 int unsigned_integer(va_list);
-int print_unsgined_number(unsigned int);
 int parser(const char *format, specifier fun_list[], va_list arg_list);
 int write_char(char c);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+/*additional functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
 #endif
