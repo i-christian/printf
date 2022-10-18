573ae167e1d18a8f03a4b837e6dd0146ff8a623c
Christian <111076892+i-christian@users.noreply.github.com>
2022-10-18 17:09:15 +0200

    Update base_changes.c

--- base_changes.c
+++ base_changes.c
@@ -11,4 +11,33 @@ int print_binary(va_list list)
 {
 	unsigned int num;
 	int i, len;
-	ch
+	char *str;
+	char *rev_str;
+
+	num = va_arg(list, unsigned int);
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 2);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (-1);
+
+	for (i = 0; num > 0; i++)
+	{
+		if (num % 2 == 0)
+			str[i] = '0';
+		else
+			str[i] = '1';
+		num = num / 2;
+	}
+	str[i] = '\0';
+	rev_str = rev_string(str);
+	if (rev_str == NULL)
+		return (-1);
+	write_base(rev_str);
+	free(str);
+	free(rev_str);
+	return (len);
+}
