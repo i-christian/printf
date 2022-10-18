2297873f49c0fd6c81e1aa86270a205ca71ca634
honorifia <40751841+honorifia@users.noreply.github.com>
2022-10-18 21:28:36 +0200

    group project

--- _printf.c
+++ _printf.c
@@ -22,8 +22,7 @@ int _printf(const char *format, ...)
 		{"R", rot13},
 		{"u", unsigned_integer},
 		{"o", print_octal},
-		{"x", print_hex},
-		{'p', print_address},
+		/*{"x", print_hex},*/
 		{"X", print_heX},
 		{NULL, NULL}
 	};
