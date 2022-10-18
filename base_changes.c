aa45e0d0733bcc3ff2b38040f5087e012f0357e7
honorifica <mkajiwonele@must.ac.mw>
2022-10-18 08:05:04 -0700

    print binary

---
+++ base_changes.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ *print_binary - Converts a number from base 10 to binary
+ *@list: List of arguments passed to this function
+ *Return: The length of the number printed
+ */
+
+int print_binary(va_list list)
+{
+	unsigned int num;
+	int i, len;
+	ch
--- main.h
+++ main.h
@@ -23,6 +23,7 @@ int print_string(va_list);
 int print_percent(va_list);
 int print_integer(va_list list);
 int print_number(va_list);
+int print_binary(va_list);
 int unsigned_integer(va_list);
 int print_unsgined_number(unsigned int);
 int parser(const char *format, specifier fun_list[], va_list arg_list);
